2d14d462b4a19fbf8d190eccca5296405893cb6f
12amankumar <117712947+12amankumar@users.noreply.github.com>
2023-01-17 11:03:23 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let textEl=document.querySelector("#evaluatedText");
+let counterEl=document.querySelector("#letterCount");
+ 
+textEl.addEventListener("input",text);
+
+function text(){
+    let word=evaluatedText.value;
+    let wordlength=word.length;
+    letterCount.innerHTML= wordlength;
+}
